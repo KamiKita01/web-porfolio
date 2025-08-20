93dc90d8a211dc2304335b5ccba81cfcc8d9f643
KamiKita01 <138034879+KamiKita01@users.noreply.github.com>
2025-08-20 15:05:47 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -176,29 +176,23 @@
 	
 
 	var counter = function() {
-		
-		$('#section-counter, .hero-wrap, .ftco-counter, .ftco-about').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-
-				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-				$('.number').each(function(){
-					var $this = $(this),
-						num = $this.data('number');
-						console.log(num);
-					$this.animateNumber(
-					  {
-					    number: num,
-					    numberStep: comma_separator_number_step
-					  }, 7000
-					);
-				});
-				
-			}
-
-		} , { offset: '95%' } );
-
-	}
+    $('#section-counter, .hero-wrap, .ftco-counter, .ftco-about').waypoint( function( direction ) {
+        if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+            var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+            $('.number').each(function(){
+                var $this = $(this),
+                    num = $this.data('number');
+                console.log(num);
+                $this.animateNumber(
+                    {
+                        number: num,
+                        numberStep: comma_separator_number_step
+                    }, 300 // 7초에서 0.3초로 변경
+                );
+            });
+        }
+    }, { offset: '95%' } );
+}
 	counter();
 
 
